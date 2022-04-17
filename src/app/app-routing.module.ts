b15619bfd19a5440d5d9162b7c802f09622ecb6d
germanvalentin <germanvalentin27@gmail.com>
2022-04-17 17:39:09 -0300

    Ion toast implement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'pages',
     loadChildren: () => import('./pages/pages.module').then( m => m.PagesPageModule)
   },
+  {
+    path: 'toast',
+    loadChildren: () => import('./pages/toast/toast.module').then( m => m.ToastPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/pages/toast/toast.page.ts
+++ src/app/pages/toast/toast.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-toast',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToastPage implements OnInit {
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
 
   ngOnInit() {
+    this.presentToast();
+  }
+ 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Clase desarrollos aplicativos.',
+      duration: 4000
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      icon: 'information-circle',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    await toast.present();
+
+    const { role } = await toast.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
 }
+
